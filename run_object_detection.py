272a0bc3a028c00d4b9d141dfb322466cb36a144
aetaffe <162856650+aetaffe@users.noreply.github.com>
2024-05-28 14:53:41 -0700

    Update run_object_detection.py
    removed access token

--- run_object_detection.py
+++ run_object_detection.py
@@ -44,7 +44,6 @@ from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
 
-access_token = "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"
 logger = logging.getLogger(__name__)
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
